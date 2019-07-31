377b99909d10e495b4fb3494de8190ca480bac58
ivanduarte99 <52494247+ivanduarte99@users.noreply.github.com>
2019-07-31 15:26:09 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,22 +1,25 @@
 function mostrar()
 {
-    numero=prompt("Ingrese un número");
-    numero=parseInt(numero);        
-    primo=true;
+    //Conseguir lo datos
+    numero = parseInt(prompt("Ingrese un número"));
+    contador = 0;
 
-    for(i = 1 ; i < numero ; i++)
-    {
-        if(numero % i == 0 && i != 1)
-        {
-            primo=false;
+    //Loop hasta que i iguale al número ingresado
+    for (i = 1 ; i <= numero ; i++) {
+        //Calcular si el numero es divisible por sí mismo y uno
+        //El modulo % nos va a dar lo que sobre de la operación
+        //Y si es 0 es porque el número es divisible
+        if (numero % i == 0) {
+            //Incrementamos el contador
+            contador ++;
         }
     }
 
-    if(primo == true)
-    {
-        console.log("El número es primo");
-    }else
-    {
-        console.log("El número no es primo");
+    //Si el valor del contador es 2 entonces el número es primo
+    //Porque los números primos son divisibles por 2 números, si mismo y 1
+    if (contador == 2) {
+        console.log("Número primo");
+    }else{
+        console.log("Número no primo");
     }
 }//FIN DE LA FUNCIÓN
