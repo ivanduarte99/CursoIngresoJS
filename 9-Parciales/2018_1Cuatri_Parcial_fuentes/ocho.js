413a80138573810cb30eff83477df4b10993050c
ivanduarte99 <52494247+ivanduarte99@users.noreply.github.com>
2019-08-04 17:42:36 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,86 @@
 function mostrar()
 {
+    var contador = 0;
+    var cantidadPares = 0;
+    var cantidadImpares = 0;
+    var cantidadCeros = 0;
+    var sumaPositivos = 0;
+    var sumaNegativos = 0;
+    var respuesta = "si";
+    var numeroMenor;
+    var numeroMayor;
+    var letraMenor;
+    var letraMayor;
 
+    while(respuesta == "si")
+    {
+        contador++;
+
+        letra = prompt("Ingrese una letra");
+
+        numero = parseInt(prompt("Ingrese un número entre -100 y 100"));
+
+        while (isNaN(numero) || numero < -100 || numero > 100)
+		{
+			numero = prompt("Número inválido, intente nuevamente");
+        }
+        
+        if (numero % 2 == 0 && numero != 0)
+        {
+            cantidadPares++;
+        }
+
+        if (numero % 2 == 1)
+        {
+            cantidadImpares++;
+        }
+
+        if (numero == 0)
+        {
+            cantidadCeros++;
+        }
+
+        if (numero > 0)
+        {
+            sumaPositivos += numero;
+        }
+
+        if (numero < 0)
+        {
+            sumaNegativos += numero;
+        }
+
+        if (contador == 1)
+		{
+            numeroMenor = numero;
+            letraMenor = letra;
+            numeroMayor = numero;
+            letraMenor = letra;
+        }
+
+        if (numero < numeroMenor)
+        {
+            numeroMenor = numero;
+            letraMenor = letra;
+            informeMinimo = numeroMenor + ", " + letraMenor;
+        }
+
+        if (numero > numeroMayor)
+        {
+            numeroMayor = numero;
+            letraMayor = letra;
+            informeMaximo = numeroMayor + ", " + letraMayor;
+        }
+
+        respuesta = prompt("Desea ingresar un número?");
+    }
+
+    promedioPositivos = sumaPositivos / contador;
+
+    console.log("Cantidad de números pares: " + cantidadPares);
+    console.log("Cantidad de números impares: " + cantidadImpares);
+    console.log("Cantidad de ceros: " + cantidadCeros);
+    console.log("Promedio de los números positivos: " + promedioPositivos);
+    console.log("Suma de todos los números negativos: " + sumaNegativos);
+    console.log("Dato con valor mayor: " + informeMaximo + " y dato de menor valor: " + informeMinimo); 
 }
